4ea6f906bb6ac73c588620a3bb5cd6707046b322
Carlos Loureda <carloslouredaparrado@gmail.com>
2021-05-23 06:41:14 +0200

    design: Minor fix on nav responsive design

--- lib/components/Layout/Header/index.tsx
+++ lib/components/Layout/Header/index.tsx
@@ -5,7 +5,7 @@ import { Menu } from './Menu'
 const Header = (): JSX.Element => {
   return (
     <header className="py-4 bg-black">
-      <div className="container flex items-center justify-between">
+      <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold text-white uppercase pl-4">
           <Link href="/">
             <a className="text-white hover:text-white hover:no-underline">Headless Wordpress</a>
