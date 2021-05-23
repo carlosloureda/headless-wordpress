b1c95fa67efa3d1e5d91a992491a44a9f3c4b0d7
Carlos Loureda <carloslouredaparrado@gmail.com>
2021-05-23 02:35:06 +0200

    fix(seo|webpack5): Title & description

--- lib/components/Layout/index.tsx
+++ lib/components/Layout/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Head from 'next/head'
+
 import Header from './Header'
 import Footer from './Footer'
 
 const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
     <div className="flex flex-col flex-1 min-h-screen">
+      <Head>
+        <title>Wordpress Headless</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Boilerplate for Headless Wordpress using NextJS" />
+      </Head>
       <Header />
       <main
         className="flex flex-row flex-1"
